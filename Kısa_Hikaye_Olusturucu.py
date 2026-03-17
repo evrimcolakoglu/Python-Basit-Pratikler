d80a4949c3cb2e3a88c556890b6b419ec5725199
Evrim Çolakoğlu <p4rgelievc0p4sha305190328@outlook.com>
2026-03-17 22:15:13 +0300

    Otomatik g├╝ncelleme

--- Kısa_Hikaye_Olusturucu.py
+++ Kısa_Hikaye_Olusturucu.py
@@ -1,4 +1,6 @@
-karakter = input("Karakter Adı: ")
-sehir = input("Olayların Geçtiği Şehir: ")
 
-print(f"{karakter} benim adım. {sehir}'da yaşıyorum. {karakter}, {sehir}'de yaşıyor.")
+karakter = input("Karakter Adı: ") #karakter isimli bir değişken tutarak kullanacağımız adın ataması yapılır.
+sehir = input("Olayların Geçtiği Şehir: ") #sehir isimli bir değişken tutarak kullanacağımız şehrin ataması yapılır.
+
+print(f"{karakter} benim adım. {sehir}'da yaşıyorum. {karakter}, {sehir}'de yaşıyor.") 
+#print metodunda f-string kullanarak değişkenlerin metin içinde süslü parantez içinde ataması sağlanır.
---
+++ While_Hesap_Makinesi.py
@@ -0,0 +1,34 @@
+""" 
+PYTHON PROJE #5
+- - - - - - - -
+Kullanıcının iki sayı girerek toplama, çıkarma, çarpma ve bölme işlemlerini yapabileceği bir
+hesap makinesi programı yazın. Program şu özelliklere sahip olmalıdır:
+
+Kullanıcıya yapmak istediği işlemi seçmesi için bir menü sunulmalıdır:
+
+1. Toplama
+2. Çıkarma
+3. Çarpma
+4. Bölme
+5. Çıkış
+Kullanıcı geçerli bir işlem seçtikten sonra iki sayı girmesi istenmelidir.
+
+İşlem sonucu ekrana yazdırılmalıdır.
+
+Kullanıcı 5 girerek çıkış yapmadıkça program sürekli çalışmalıdır.
+
+Bölme işleminde sıfıra bölme durumu kontrol edilmelidir.
+"""
+
+input(
+"""
+Aşağıdaki işlemlerden birisini seçiniz:
+
+1- Toplama
+2- Çıkarma
+3- Bölme
+4- Çarpma
+5- Çıkış
+
+"""
+)
