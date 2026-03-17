8ea78e8430b93f96fe5c1c1bc82f9af017d366c3
Evrim Çolakoğlu <p4rgelievc0p4sha305190328@outlook.com>
2026-03-17 22:54:21 +0300

    Gerekli kodlar eklendi.

--- While_Hesap_Makinesi.py
+++ While_Hesap_Makinesi.py
@@ -20,15 +20,48 @@ Kullanıcı 5 girerek çıkış yapmadıkça program sürekli çalışmalıdır.
 Bölme işleminde sıfıra bölme durumu kontrol edilmelidir.
 """
 
-input(
-"""
-Aşağıdaki işlemlerden birisini seçiniz:
+while True:
+    print(
+    """
+    While Hesap Makinesi
 
-1- Toplama
-2- Çıkarma
-3- Bölme
-4- Çarpma
-5- Çıkış
+    1- Toplama
+    2- Çıkarma
+    3- Bölme
+    4- Çarpma
+    5- Üs Alma
+    6- Çıkış
 
-"""
-)
+    """
+    )
+
+    secim = input("(1-6) arasında bir sayı giriniz: ")
+
+    if secim == "6":
+        "Programdan başarıyla çıkıldı."
+        break
+
+    if secim in ["1","2","3","4","5"]:
+        sayi1 = float(input("Birinci sayıyı giriniz: "))
+        sayi2 = float(input("İkinci sayıyı giriniz: "))
+
+        if secim == "1":
+            print(sayi1+sayi2)
+            break
+        if secim == "2":
+            print(sayi1-sayi2)
+            break
+        if secim == "3":
+            print(sayi1/sayi2)
+            break
+        if secim == "4":
+            print(sayi1*sayi2)
+            break
+        if secim == "5":
+            print(sayi1**sayi2)
+            break    
+
+    else:
+        print("Geçersiz seçim, lütfen 1-6 arasında bir değer giriniz: ")
+    
+
